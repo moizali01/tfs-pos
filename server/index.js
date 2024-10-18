2780ab5f359723951b765d4a7d5b59798f43bb9b
Moiz Ali <moiz@Moizs-MacBook-Air.local>
2024-10-18 17:41:41 +0500

    path

--- server/index.js
+++ server/index.js
@@ -26,10 +26,10 @@ app.use("/api/bills", billsRoutes);
 
 
 // static files
-app.use(express.static(path.join(__dirname,'./build')))
+app.use(express.static(path.join(__dirname,'../client/build')))
 
 app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname, './build/index.html'))
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
 
 const PORT = process.env.PORT || 4001;
